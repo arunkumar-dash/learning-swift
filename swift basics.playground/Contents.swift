0b6b679eb754acf4e2fbf90f2351ca3bf881b5fc
arunkumar-dash <arun.kumar.dash77@gmail.com>
2021-08-31 17:36:41 +0530

    switch cases

--- swift basics.playground/Contents.swift
+++ swift basics.playground/Contents.swift
@@ -65,3 +65,41 @@ for i in arrayOfArrays{
         print(j)
     }
 }
+
+var set: Set<Int> = Set([23,56])
+set.insert(12)
+var set1 : Set = ["ad","assa","lkk","s"]
+
+set1.sorted{ $0.count > $1.count}
+print(set1)
+
+var dict1: Dictionary<Int, Array<String>> = [1:["78","idiom","pojo"],78:["0","\u{1F159}"]]
+for (i,j) in dict1{
+    print("Key \(i) \t Value \(j)")
+}
+
+//stride
+
+for i in stride(from: 0, to: 10, by: 2){
+    print(i)
+}
+
+//switch case
+switch "dinesh" {
+case "dinesh":
+    print("case 1")
+case "arun":
+    print("case 2")
+default:
+    print("default")
+}
+
+switch (1,1) {
+case (_,1):
+    print("case 1")
+case (1,_):
+    print("case 2")
+default:
+    print("default")
+}
+
