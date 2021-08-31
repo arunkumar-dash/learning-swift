822455f1f54fac7af463cd24031ca224993e7d05
arunkumar-dash <arun.kumar.dash77@gmail.com>
2021-08-31 11:50:43 +0530

    ternary operators

--- swift basics.playground/Contents.swift
+++ swift basics.playground/Contents.swift
@@ -1,8 +1,15 @@
 import UIKit
 
+
+//Testing commits
 var greeting = "Hello, playground"
 let a = 20
 let b = 45.566
 print(Double(a)+b)
 var c: Int = 9000
 print(a&c)
+
+//tuples and ternary operator
+var (x,y,z)=(1,2,3)
+print(x == y ? "x equals y" : (y == z ? "y equals z" : (x == z ? "x equals z" : "nothing matches")))
+
