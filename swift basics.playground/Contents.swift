4958a5a0eb05b0e648152a2d1e929f9d8b877ec0
arunkumar-dash <arun.kumar.dash77@gmail.com>
2021-08-31 16:18:45 +0530

    arrays

--- swift basics.playground/Contents.swift
+++ swift basics.playground/Contents.swift
@@ -31,7 +31,37 @@ for _ in 1..<5 {
     print("hii")
 }
 
-let arr : [Int] = [1, 2, 3]
+let arr : [String] = ["a", "b", "c"]
 for i in 0..<arr.count{
     print(arr[i])
+}
+for i in arr[0...]{
+    print(i)
+}
+
+//read user inputs
+/*
+var aReadLine = readLine()
+if let aInt = aReadLine {
+    print("You typed \(aInt)")
+}
+else {
+    print(#"nothing was typed \u{1F319}"#)
+    print("nothing was typed \u{1F319}")
+}
+ */
+
+//startIndex
+var aString: String = "Hello World"
+print(aString[aString.startIndex], aString[aString.index(before: aString.endIndex)], aString[aString.index(after: aString.startIndex)])
+
+print(aString.hasPrefix("Hell"))
+
+//Collections
+var array: [String] = Array(repeating: "panda", count: 12)
+var arrayOfArrays: [[Int]] = [[89,9],[12,123]]
+for i in arrayOfArrays{
+    for j in i.reversed(){
+        print(j)
+    }
 }
